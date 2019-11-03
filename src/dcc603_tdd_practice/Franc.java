4163a94417a228e741db031c27d3780fee5873c3
jonatascavalcante <jonatascavalcante777@gmail.com>
2019-11-03 10:23:06 -0300

    step 4

--- src/dcc603_tdd_practice/Franc.java
+++ src/dcc603_tdd_practice/Franc.java
@@ -1,10 +1,13 @@
 package dcc603_tdd_practice;
 
 public class Franc extends Money {
-	Franc(int amount) {
-		this.amount = amount;
+	private String currency;
+	
+	Franc(int amount, String currency) {
+		super(amount, currency);
 	}
+	
 	Money times(int multiplier) {
-		return new Franc(amount * multiplier);
+		return Money.franc(amount * multiplier);
 	}
 }
